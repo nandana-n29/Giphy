bbfe74b1eb17bd67cd1757d55e231952e0a1e6a3
Nandana Nair <nandananair2906@gmail.com>
2025-08-16 12:59:24 +0530

    1 new file added

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Tailwind v3 + Vite 5 works! 🚀
+      </h1>
+    </div>
+  )
+}
